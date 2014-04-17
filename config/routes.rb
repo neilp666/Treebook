4f4d2a9ec5ce2374dadd0ea9a30a21b8dfc7146f
neil patel <neil@myindianweddingapp.com>
2014-04-17 20:30:11 +0100

    Install devise and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Treebook::Application.routes.draw do
+  devise_for :users
   resources :statuses
   root 'statuses#index'
   # The priority is based upon order of creation: first created -> highest priority.
