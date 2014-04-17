f3f1fc3530d00790440a763db026754d453d9529
neil patel <neil@myindianweddingapp.com>
2014-04-17 10:36:39 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+== README
+
+Treehouse application 
+
+* Ruby version 2.0 
+
+* Rails 4.0.0
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Treebook::Application.config.secret_key_base = '9eeb96d82ffd0cc1bb5873177583b26b5dab6442f78f301ded8e276b7a69e1934dd00c1b421d4752494bf866337e1f80ede5dcf86c4e869157380f7914df3d26'
